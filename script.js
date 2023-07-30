74af53648e6059f152a70f2f4f730830d62271cb
ajitshende123 <87682343+ajitshende123@users.noreply.github.com>
2023-07-30 11:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+document.body.onload = createBoxes();
+
+function createBoxes() {
+  const container = document.getElementById("container");
+  for (let i = 1; i <= 800; i++) {
+    const box = document.createElement("div");
+    const id = `box${i}`;
+    box.className = "square";
+    box.id = id;
+    box.onmouseover = () => handleHoverEnter(id);
+    box.onmouseout = () => handleHoverLeave(id);
+    container.appendChild(box);
+  }
+}
+
+function handleHoverEnter(id) {
+  const box = document.getElementById(id);
+  clearTimeout(box.dataset.timer);
+  delete box.dataset.timer;
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  box.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+function handleHoverLeave(id) {
+  const box = document.getElementById(id);
+  box.dataset.timer = setTimeout(() => {
+    box.style.backgroundColor = "rgb(29, 29, 29)";
+    delete box.dataset.timer;
+  }, 1000);
+}
